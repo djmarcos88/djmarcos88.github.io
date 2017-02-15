8ff08226ee3bbe39905a4f886c311b2e4fad8a6d
Marcos Djivelekian <djmarcos88@gmail.com>
2017-02-15 12:47:43 -0300

    Added an animation to the page.

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    var profilePic = document.querySelector('.profile-pic');
+    var faders = document.querySelector('.faders');
+
+    setTimeout(function() {
+        profilePic.className += ' fade';    
+    }, 500);
+
+    setTimeout(function() {
+        faders.className += ' fade';
+    }, 1000);
+};
